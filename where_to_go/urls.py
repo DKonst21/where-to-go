6316913baf41ae5ec0b0ced5f5f40749ec58977f
DKonst21 <104372191+DKonst21@users.noreply.github.com>
2023-09-13 23:48:38 +0300

    Add model for images

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -14,14 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from where_to_go import views
+from django.contrib import admin
+from django.urls import path, include
 
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.show_phones),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('', views.show_main),
+                  path('places/<int:place_id>/', views.place_view, name='places'),
+                  path('tinymce/', include('tinymce.urls')),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,4 +1,47 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
-def show_phones(request):
-    return render(request, 'index.html')
+from places.models import Place
+
+
+def show_main(request):
+    locations = Place.objects.all()
+    features = []
+    for location in locations:
+        formatted_location = {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [float(location.lng), float(location.lat)]
+            },
+            'properties': {
+                'title': location.title,
+                'placeId': location.id,
+                'detailsUrl': reverse('places', args=[location.id]),
+            }
+        }
+        features.append(formatted_location)
+
+    context = {
+        'geojson': {
+            'type': 'FeatureCollection',
+            'features': features,
+        },
+    }
+    return render(request, 'index.html', context=context)
+
+
+def place_view(request, place_id):
+    place = get_object_or_404(Place, pk=place_id)
+    context = {
+        'title': place.title,
+        'imgs': [image.image.url for image in place.images.all()],
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates': {
+            'lat': place.lat,
+            'lng': place.lng,
+        }
+    }
+    return JsonResponse(context, safe=False, json_dumps_params={'indent': 2, 'ensure_ascii': False})
