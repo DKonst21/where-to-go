9a934d32e976f9e757455b2bebc15cf6ac5f48a5
DKonst21 <104372191+DKonst21@users.noreply.github.com>
2023-10-14 19:00:27 +0300

    "Position" field made optional

--- places/models.py
+++ places/models.py
@@ -48,7 +48,7 @@ class Image(models.Model):
 
     image = models.ImageField('Изображение')
 
-    position = models.IntegerField('Позиция', db_index=True)
+    position = models.IntegerField('Позиция', default=1, db_index=True)
 
     class Meta:
         verbose_name = 'изображение'
