d33778a7bb01b131b3176bf7e166da781a3d400e
DKonst21 <104372191+DKonst21@users.noreply.github.com>
2023-10-15 15:40:24 +0300

    Removed commented text

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -30,8 +30,6 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default='False')
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost', '[::1]'])
-# ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
-
 
 # Application definition
 
